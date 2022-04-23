bde6055cd46cf4416319641ad8608842f653445a
Renato Xavier <rnatu91@gmail.com>
2022-04-22 23:12:15 -0300

    feat: add class statefull component

--- 01-react-basic/src/App.js
+++ 01-react-basic/src/App.js
@@ -24,21 +24,50 @@ import React, { Component } from "react";
 // }
 
 class App extends Component /* or React.Component */ {
+  constructor(props) {
+    super(props);
+    //binding handlePClick function
+    this.handlePClick = this.handlePClick.bind(this);
+
+    this.state = {
+      name: "Renato Xavier",
+      counter: 0,
+    };
+  }
+
+  handlePClick() {
+    this.setState({ name: "Junior" });
+  }
+
+  //using arrow function, don't need to using bind
+  handleAClick = (e) => {
+    e.preventDefault();
+
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+
+    console.log(counter);
+  };
+
   render() {
+    const { name } = this.state;
+    const { counter } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p onClick={this.handlePClick}>
+            {name} - {counter}
           </p>
           <a
+            onClick={this.handleAClick}
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Adicionar
           </a>
         </header>
       </div>
