64f980fa5715fd24f36c6ba3e27fd96e78ac53a3
Renato <rnatu91@gmail.com>
2022-07-11 16:55:01 -0300

    refactor(PostCard): change from class component to functional component

--- 01-react-basic/functional-component/src/components/PostCard/index.jsx
+++ 01-react-basic/functional-component/src/components/PostCard/index.jsx
@@ -1,20 +1,15 @@
-import { Component } from "react";
 import "./styles.css";
 
-export class PostCard extends Component {
-  render() {
-    const { cover, title, body, id } = this.props;
-
-    return (
-      <div className="post">
-        <img src={cover} alt={title} />
-        <div className="post-content">
-          <h2>
-            {title} - {id}
-          </h2>
-          <p>{body}</p>
-        </div>
+export function PostCard({ cover, title, body, id }) {
+  return (
+    <div className="post">
+      <img src={cover} alt={title} />
+      <div className="post-content">
+        <h2>
+          {title} - {id}
+        </h2>
+        <p>{body}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
