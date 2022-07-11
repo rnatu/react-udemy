9b1ca1ea144518a5bbe3b0b0c94ee7b64b34020e
Renato <rnatu91@gmail.com>
2022-07-11 16:57:57 -0300

    refactor(TextInput): change from class component to functional component

--- 01-react-basic/functional-component/src/components/TextInput/index.jsx
+++ 01-react-basic/functional-component/src/components/TextInput/index.jsx
@@ -1,20 +1,15 @@
-import { Component } from "react";
 import "./styles.css";
 
-export class TextInput extends Component {
-  render() {
-    const { searchValue, handleChange } = this.props;
-
-    return (
-      <div className="inputText-container">
-        {searchValue && <h1>Search Value: {searchValue}</h1>}
-        <input
-          type="search"
-          onChange={handleChange}
-          value={searchValue}
-          placeholder="Buscar"
-        />
-      </div>
-    );
-  }
+export function TextInput({ searchValue, handleChange }) {
+  return (
+    <div className="inputText-container">
+      {searchValue && <h1>Search Value: {searchValue}</h1>}
+      <input
+        type="search"
+        onChange={handleChange}
+        value={searchValue}
+        placeholder="Buscar"
+      />
+    </div>
+  );
 }
