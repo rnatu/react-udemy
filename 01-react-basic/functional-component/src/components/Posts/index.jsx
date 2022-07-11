8fd1499184853acc212a99738c8c5e6e0b022c76
Renato <rnatu91@gmail.com>
2022-07-11 16:56:41 -0300

    refactor(Posts): change from class component to functional component

--- 01-react-basic/functional-component/src/components/Posts/index.jsx
+++ 01-react-basic/functional-component/src/components/Posts/index.jsx
@@ -1,23 +1,18 @@
-import { Component } from "react";
 import { PostCard } from "../PostCard";
 import "./styles.css";
 
-export class Posts extends Component {
-  render() {
-    const { posts } = this.props;
-
-    return (
-      <div className="posts">
-        {posts.map((post) => (
-          <PostCard
-            key={post.id}
-            cover={post.cover}
-            title={post.title}
-            body={post.body}
-            id={post.id}
-          />
-        ))}
-      </div>
-    );
-  }
+export function Posts({ posts }) {
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <PostCard
+          key={post.id}
+          cover={post.cover}
+          title={post.title}
+          body={post.body}
+          id={post.id}
+        />
+      ))}
+    </div>
+  );
 }
